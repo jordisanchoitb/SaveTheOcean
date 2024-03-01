1e74b0022390f05b27fb5a6068b643c2c2570d64
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-03-01 18:54:52 +0100

    plantilla pujada

---
+++ UnitTesting_SaveTheOcean/UnitTesting_SaveTheOcean.cs
@@ -0,0 +1,13 @@
+using Utilities;
+
+namespace UnitTesting_SaveTheOcean
+{
+    [TestClass]
+    public class UnitTesting_SaveTheOcean
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
